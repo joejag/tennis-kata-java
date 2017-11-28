5cde2597a5bd56f393470b5afe1c3574d1b13bfd
Joe Wright <joe@joejag.com>
2017-11-28 20:39:50 +0000

    Extract methods and reuse first switch statement

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class TennisGame1 implements TennisGame {
 
@@ -19,37 +20,28 @@ public class TennisGame1 implements TennisGame {
     }
 
     public String getScore() {
-        String score = "";
-        if (m_score1 == m_score2) {
-            switch (m_score1) {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-            }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
-        } else {
-            score = basicScore(m_score1) + "-" + basicScore(m_score2);
-        }
-
-        return score;
+        if (m_score1 == m_score2) return normalScore();
+        else if (m_score1 >= 4 || m_score2 >= 4) return faceOff();
+        else return basicScore(m_score1) + "-" + basicScore(m_score2);
     }
 
-    private String basicScore(int tempScore) {
-        switch (tempScore) {
+    private String normalScore() {
+        if (Arrays.asList(0, 1, 2).contains(m_score1))
+            return basicScore(m_score1) + "-All";
+
+        return "Deuce";
+    }
+
+    private String faceOff() {
+        int minusResult = m_score1 - m_score2;
+        if (minusResult == 1) return "Advantage player1";
+        else if (minusResult == -1) return "Advantage player2";
+        else if (minusResult >= 2) return "Win for player1";
+        else return "Win for player2";
+    }
+
+    private String basicScore(int score) {
+        switch (score) {
             case 0:
                 return "Love";
             case 1:
